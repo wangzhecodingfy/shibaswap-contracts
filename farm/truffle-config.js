a87a47a0af08f4cbbe952439649076454d72c7c7
Large Zhe <92359448+wangzhecodingfy@users.noreply.github.com>
2022-08-29 01:07:57 +0800

    Update truffle-config.js

--- farm/truffle-config.js
+++ farm/truffle-config.js
@@ -29,7 +29,7 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "0.6.12",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
          enabled: true,
